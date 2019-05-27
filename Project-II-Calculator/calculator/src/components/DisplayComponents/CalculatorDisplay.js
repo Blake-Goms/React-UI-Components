b9fd4ab816eff40cb79eddbc0403c3ab636b6127
Blake Goms <blakegoms@gmail.com>
2019-05-27 16:23:49 -0700

    MVP

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = () => {
+    return <button className="display">0</button>
+};
+
+
+export default CalculatorDisplay;
